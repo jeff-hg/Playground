794468f1f7afa2f77745110967dbf6c592a17aaf
Jeffery Huang <74565907+jeff-hg@users.noreply.github.com>
2021-05-27 10:06:45 -0400

    Create kaboom_with_tkinter.py

---
+++ kaboom_with_tkinter.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+from typing import Set
+
+
+class Application(tk.Frame):
+    def __init__(self, master):
+        # We are inheriting methods from the 
+        # tkinter.Frame class and passing it the root as an arg
+        super().__init__(master)
+        self.widgets = []
+        self.setup()
+        # Pack Application object onto the window
+        self.pack()
+
+    def setup(self) -> None:
+        """ Method for setting up objects"""
+        death_button = tk.Button(self, text="DONT PRESS THIS BUTTON!", bg='red', fg='white', command=self.explosion)
+        self.widgets.append(death_button)
+        for widget in self.widgets:
+            widget.pack()
+
+    @staticmethod
+    def explosion():
+        """ This method explodes"""
+        print("KABOOM!")
+
+if __name__ == "__main__":
+    # Create window and set it to root
+    root = tk.Tk()
+    # Create Application object
+    app = Application(root)
+    # Run mainloop method from the tkinter.Frame superclass of the Application class
+    app.mainloop()
